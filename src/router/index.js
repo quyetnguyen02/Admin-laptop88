33a92c4a86847e704b8e1167786677e055363bf7
quyetnguyen02 <quyetnguyen0804@gmail.com>
2022-10-17 22:34:57 +0700

    push code layout

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from "@/views/Dashboard.vue";
+import Signin from "@/views/Signin.vue";
+import Signup from "@/views/Signup.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: "/",
       name: "Dashboard",
       component: Dashboard,
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: Signin,
+    },
+    {
+      path: "/register",
+      name: "Signup",
+      component: Signup,
     },
   ],
 });
